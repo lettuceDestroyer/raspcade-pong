ab52ff1ce74fdd91de0deb579b3d7371c363551d
lettuceDestroyer <tobiladner@hotmail.com>
2025-05-04 21:43:30 +0200

    made changes to 'main.py'

--- main.py
+++ main.py
@@ -19,15 +19,24 @@ is_game_over: bool = False
 left_paddle = Paddle(pygame.Color("red"), 20, 120, 0, 80, (HEIGHT - 120) / 2)
 right_paddle = Paddle(pygame.Color("blue"), 20, 120, 0, WIDTH - 80 - 20, (HEIGHT - 120) / 2)
 ball = Ball(pygame.Color("white"), 15, WIDTH / 2 - 15, HEIGHT / 2 - 15, 0.5, 0.5)
+score: int
+high_score: int
 
 model: any
 camera: pygame.camera.Camera
 game_over_font: pygame.font.Font
+default_font: pygame.font.Font
 
 def init():
+    global score
+    global high_score
+
     pygame.init()
     pygame.camera.init()
-    pygame.display.set_caption("PingPong")
+    pygame.display.set_caption("Raspcade Pong")
+
+    score = 0
+    high_score = 0
 
 def game_over():
     window.fill(pygame.Color("black"))
@@ -35,11 +44,51 @@ def game_over():
     window.blit(game_over_text, ((WIDTH - game_over_text.get_width()) / 2, (HEIGHT - game_over_text.get_height()) / 2))
     pygame.display.update()
 
+def reload_game():
+    global score
+
+    for count in range(3, 0, -1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        window.fill(pygame.Color("black"))
+
+        # Render the main text
+        main_text = default_font.render("Hand recognised. Game starting in", True, pygame.Color("white"))
+        window.blit(main_text, ((WIDTH - main_text.get_width()) / 2, (HEIGHT - main_text.get_height()) / 2))
+
+        # Render the countdown number
+        countdown_text = default_font.render(str(count), True, pygame.Color("white"))
+        window.blit(countdown_text, ((WIDTH - main_text.get_width()) / 2, ((HEIGHT - main_text.get_height()) / 2)) + main_text.get_height() + 10)
+
+        pygame.display.flip()
+        pygame.time.wait(1000)
+
+    score = 0
+    main()
+
 def load_fonts():
+    global game_over_font
+    global default_font
+
     try:
         game_over_font = pygame.font.Font(os.path.join("res", "fonts", "faster_one", "faster_one_regular.ttf"), 80)
     except FileNotFoundError:
         game_over_font = pygame.font.SysFont(None, 80)
+
+    try:
+        default_font = pygame.font.Font(os.path.join("res", "fonts", "robo_condensed", "RobotoCondensed-Regular.ttf"), 20)
+    except FileNotFoundError:
+        default_font = pygame.font.SysFont(None, 80)
+
+def update_score(score: int):
+    if score > high_score:
+        high_score = score
+    
+    score_text = default_font.render(f"high score: {high_score} | score: {score}", True, pygame.Color("white"))
+    window.blit(score_text, (0,0))
 
 def load_model():
     model = torch.load(MODEL_PATH, weights_only=False)
@@ -47,6 +96,9 @@ def load_model():
 
 def init_camera():
     camera_list = pygame.camera.list_cameras()
+
+    print(f"camera_list: {camera_list}")
+
     if not camera_list:
         raise ValueError("Sorry, no cameras detected.")
     camera = pygame.camera.Camera(camera_list[0])
@@ -82,6 +134,15 @@ def surfact_to_tensor(surface: pygame.Surface):
     print(tensor.shape)
     return tensor
 
+def translate_box(bbox: torch.Tensor):
+
+    xmin = bbox[0] * HEIGHT/640
+    ymin = bbox[1] * HEIGHT/640
+    xmax = bbox[2] * HEIGHT/640
+    ymax = bbox[3] * HEIGHT/640
+
+    return torch.Tensor([xmin, ymin, xmax, ymax])
+
 def main():
     global is_game_over
 
@@ -91,10 +152,17 @@ def main():
     load_model()
 
     while True:
-        if is_game_over:
-            game_over()
+        image_as_surface = camera.get_image()
+        image_as_tensor = surfact_to_tensor(image_as_surface)
+        predictions = model(image_as_tensor)
 
-        if not is_game_over:
+        bboxes = predictions[0]['boxes']
+    
+        if is_game_over and len(bboxes) <= 0:
+            game_over()
+        elif is_game_over and len(bboxes) >= 1:
+            reload_game()
+        else:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.camera.quit()
@@ -108,14 +176,14 @@ def main():
                     right_paddle.velocity = 0
                     right_paddle.velocity = 0
 
-            image_as_surface = camera.get_image()
-            image_as_tensor = surfact_to_tensor(image_as_surface)
-            predictions = model(image_as_tensor)
 
-            boxes = predictions[0]['boxes']
-            labels_as_tensor = predictions[0]['labels']
-            scores = predictions[0]['scores']
 
+            bbox: torch.Tensor = None
+            if len(bboxes) > 0:
+                bbox = translate_box(bboxes[0])
+
+                y = (bbox[1] + bbox[3])/2
+                left_paddle.y = y - left_paddle.height / 2
 
             # ball movement controls
             if ball.x <= 0 or ball.x + ball.radius * 2 >= WIDTH:
@@ -140,6 +208,7 @@ def main():
                 ball.x_velocity *= -1
 
             if pygame.sprite.collide_rect(ball, right_paddle):
+                score += 1
                 ball.x_velocity *= -1
 
             # move left paddle based on ball location
@@ -162,6 +231,9 @@ def main():
             ball.draw(window)
             left_paddle.draw(window)
             right_paddle.draw(window)
+
+            # update score
+            update_score()
 
             # update screen
             pygame.display.update()
